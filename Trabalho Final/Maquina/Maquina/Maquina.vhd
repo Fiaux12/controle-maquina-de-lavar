7cd7115e7aa52da17b984ff5eb0a415a1998eab9
elainelima <elainebarbosalimaa@gmail.com>
2025-01-24 13:51:04 -0300

    alterando codigo da maquina 2

--- Trabalho Final/Maquina/Maquina/Maquina.vhd
+++ Trabalho Final/Maquina/Maquina/Maquina.vhd
@@ -17,127 +17,75 @@ entity Maquina is
 		Led_Centrifuga: out std_logic;
 		Led_Finalizado: out std_logic;
 		Valvula_Agua: out std_logic;
-		Load_Motor: out std_logic;
+		Load_Motor: out std_logic
 
    );
 end Maquina;
 architecture FSM of Maquina is
- 
-
-component Datapath is
-	generic
-	(
-		DATA_WIDTH : natural := 4
-	);
-	port( 
-		Volume_Agua: in std_logic_vector((DATA_WIDTH-1) downto 0); 
-		Modo_Lavagem: in std_logic_vector((DATA_WIDTH-1) downto 0); 
-		clock: in std_logic; 
-		
-		Reset_Vol_Agua: in std_logic;
-		Load_Vol_Agua: in std_logic;
-		
-		Reset_Modo: in std_logic;
-		Load_Modo: in std_logic;
-		
-		Load_temp: in std_logic;
-		Reset_Cont: in std_logic;
-		
-		Timeout: out std_logic;
-		
-		pino_extra_baixo: out std_logic;
-		pino_baixo: out std_logic;
-		pino_medio: out std_logic;
-		pino_alto: out std_logic;
-		
-		pino_economico: out std_logic;
-		pino_delicado: out std_logic;
-		pino_pesado: out std_logic;
-		pino_normal: out std_logic;
-		
-		BCD: out std_logic_vector(6 downto 0)
-		
-   );
-end component;
-	
-component Controladora is
-	port (
-        RESET   			: in    std_logic; -- reset input
-		CLOCK   			: in    std_logic; -- clock input
-		Ligar_maquina     : in    std_logic;
-		Porta     			:in    std_logic;
-		Reset_Vol_Agua    : out   std_logic;
-		Load_Vol_Agua     : out   std_logic;
-		Reset_Modo        : out   std_logic;
-		Load_Modo				: out   std_logic;
-		Load_temp				: out   std_logic;
-		Reset_Cont			: out   std_logic;
-		Timeout				: in std_logic;
-		pino_extra_baixo  : in std_logic;
-		pino_baixo			: in std_logic;
-		pino_medio			: in std_logic;
-		pino_alto			: in std_logic;
-
-		pino_economico		: in std_logic;
-		pino_delicado		: in std_logic;
-		pino_pesado			: in std_logic;
-		pino_normal			: in std_logic;
-);
-end component;
-
---signal Load_E, Reset_MA, Maior, Menor, Descendo, Subindo, Atualize : std_logic;
 
 begin 
+	-- Instância da Controladora
 	instancia_controladora: Controladora
 		port map(
-			RESET=>RESET,
-			CLOCK=>CLOCK,
-			Ligar_maquina=>Ligar_maquina,
-			Porta=>Porta,
-			Reset_Vol_Agua=>
-			Load_Vol_Agua=>
-			Reset_Modo=>
-			Load_Modo=>
-			Load_temp=>Load_temp,
-			Reset_Cont=>Reset_Cont
-			Timeout	=>Timeout,
-			pino_extra_baixo =>pino_extra_baixo
-			pino_baixo=>pino_baixo
-			pino_medio=>pino_medio
-			pino_alto=>pino_alto
+			Ligar_maquina   => Ligar_maquina,
+			Porta           => Porta,
+			RESET           => RESET, 
+			CLOCK           => CLOCK,
+			Reset_Vol_Agua  => Reset_Vol_Agua,
+			Load_Vol_Agua   => Load_Vol_Agua,
+			Reset_Modo      => Reset_Modo,
+			Load_Modo       => Load_Modo,
+			Load_temp       => Load_temp,
+			Reset_Cont      => Reset_Cont,
+			Timeout         => Timeout,
+			pino_extra_baixo=> pino_extra_baixo,
+			pino_baixo      => pino_baixo,
+			pino_medio      => pino_medio,
+			pino_alto       => pino_alto,
+			pino_economico  => pino_economico,
+			pino_delicado   => pino_delicado,
+			pino_pesado     => pino_pesado,
+			pino_normal     => pino_normal,
+			Load_Motor      => Load_Motor,
+			Led_Enchendo    => Led_Enchendo,
+			Led_Molho       => Led_Molho,
+			Led_Lavar       => Led_Lavar,
+			Led_Enxague     => Led_Enxague,
+			Led_Centrifuga  => Led_Centrifuga,
+			Led_Finalizado  => Led_Finalizado,
+			Valvula_Agua    => Valvula_Agua
 
-			pino_economico=>pino_economico,
-			pino_delicado=>pino_delicado
-			pino_pesado=>pino_pesado
-			pino_normal=>pino_normal
 		);
+
+	-- Instância do Datapath
 	instance_datapath: Datapath
 		port map(
-			Volume_Agua=>vol_agua,
-			Modo_Lavagem=>modo_lavgm,
-			clock=>CLOCK,
+			Volume_Agua     => vol_agua,
+			Modo_Lavagem    => modo_lavgm,
+			clock           => CLOCK,
 			
-			Reset_Vol_Agua=>
-			Load_Vol_Agua=>,
+			Reset_Vol_Agua  => Reset_Vol_Agua,
+			Load_Vol_Agua   => Load_Vol_Agua,
 			
-			Reset_Modo=>
-			Load_Modo=>
+			Reset_Modo      => Reset_Modo,
+			Load_Modo       => Load_Modo,
 			
-			Load_temp=>
-			Reset_Cont=>
+			Load_temp       => Load_temp,
+			Reset_Cont      => Reset_Cont,
 			
-			Timeout=>
+			Timeout         => Timeout,
 			
-			pino_extra_baixo=>
-			pino_baixo=>
-			pino_medio=>
-			pino_alto=>
+			pino_extra_baixo=> pino_extra_baixo,
+			pino_baixo      => pino_baixo,
+			pino_medio      => pino_medio,
+			pino_alto       => pino_alto,
 			
-			pino_economico=>
-			pino_delicado=>
-			pino_pesado=>
-			pino_normal=>
+			pino_economico  => pino_economico,
+			pino_delicado   => pino_delicado,
+			pino_pesado     => pino_pesado,
+			pino_normal     => pino_normal,
+			
+			BCD             => Display_7seg
 		);
-	
-	
+
 end FSM;
